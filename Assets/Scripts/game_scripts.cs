5c841572ee45182eac130f935aeb77cc5edd6b48
aj3x <ajrussell94@hotmail.com>
2015-10-12 03:05:08 -0600

    Public scripts
    Added a script for public variables and commands

---
+++ Assets/Scripts/game_scripts.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class game_scripts : MonoBehaviour {
+    int lives;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void text() {
+
+    }
+}
