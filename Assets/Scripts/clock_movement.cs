787e7f27faf138b0c8489ed06aa77a917d375888
aj3x <ajrussell94@hotmail.com>
2015-10-11 19:37:55 -0600

    Time and lives HUD
    Added a set of sprites used for a clock, tells user current in-game
    time.
    Added sprite of pocket watch to keep track of number of lives
    Added script for clock to rotate based on current time

---
+++ Assets/Scripts/clock_movement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class clock_movement : MonoBehaviour {
+    // 5 minutes is 1 unit
+    // 1 hour is 12 units
+    public int startTime;
+    public int currentTime;
+    public GameObject hour_hand;
+    public GameObject min_hand;
+	// Use this for initialization
+	void Start () {
+        currentTime = startTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+
+    public void addTime(int num) {
+        currentTime += num;
+        updateTime();
+    }
+
+    public int timeLeft(){
+        return 144 - currentTime;
+    }
+
+    void updateTime() {
+        min_hand.transform.eulerAngles = new Vector3(0, 0, -currentTime % 12 * 30);
+        hour_hand.transform.eulerAngles = new Vector3(0, 0, -currentTime * 30/12);
+    }
+}
