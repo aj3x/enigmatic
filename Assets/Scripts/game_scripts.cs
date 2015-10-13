515d2d66bd33cb22b617852c8f30c8168b308f5e
aj3x <ajrussell94@hotmail.com>
2015-10-12 23:23:29 -0600

    Messaging System
    Added scripts to show and hide written messages

--- Assets/Scripts/game_scripts.cs
+++ Assets/Scripts/game_scripts.cs
@@ -1,19 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class game_scripts : MonoBehaviour {
+    public bool blolz;
+    public int lolz;
     int lives;
+    public GameObject textBlock;
 	// Use this for initialization
 	void Start () {
-	
-	}
+        textBlock = GameObject.Find("Text");
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 
-    void text() {
+    public void showText(string message) {
+        textBlock.GetComponent<Text>().text = message;
+        textBlock.SetActive(true);
 
     }
+
+    public void closeText() {
+        textBlock.SetActive(false);
+    }
+
 }
