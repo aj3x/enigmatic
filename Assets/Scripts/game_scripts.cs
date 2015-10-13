86c4c58caf0a6ba1fd1ffae87acb18c301f147e5
aj3x <ajrussell94@hotmail.com>
2015-10-13 00:06:28 -0600

    Title Page
    Added Page which will later include difficulty menu
    Separation of functions and saves needed
    Included script to change lives
    Saved HUD and Script object as Prefabs

--- Assets/Scripts/game_scripts.cs
+++ Assets/Scripts/game_scripts.cs
@@ -3,20 +3,42 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class game_scripts : MonoBehaviour {
+    void Awake() {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+    //debugging variables
     public bool blolz;
     public int lolz;
+    //standard variables
+    int difficulty;
     int lives;
-    public GameObject textBlock;
+    clock_movement clockScript;
+    GameObject textBlock;
+    Text livesHUD;
 	// Use this for initialization
 	void Start () {
+        //Find the Text object and close it
         textBlock = GameObject.Find("Text");
+        closeText();
+        //get scripts and such
+        livesHUD = GameObject.Find("lifeText").GetComponent<Text>();
+        clockScript = GameObject.Find("Clock").GetComponent<clock_movement>();
+
+        //adjust variables based on difficulty
+
+
+
+
+
+        updateHUD();
     }
 	
 	// Update is called once per frame
 	void Update () {
-
+        updateHUD();
 	}
 
+    //Text functions
     public void showText(string message) {
         textBlock.GetComponent<Text>().text = message;
         textBlock.SetActive(true);
@@ -27,4 +49,24 @@ public class game_scripts : MonoBehaviour {
         textBlock.SetActive(false);
     }
 
+
+
+    //HUD refresher
+    public void updateHUD() {
+        livesHUD.text = "x" + lives;
+        clockScript.SendMessage("updateTime");
+    }
+
+
+
+
+    public void loseLife() {
+        if (lives > 0) {
+            lives--;
+            GameObject.Find("livesText").GetComponent<Text>().text = "x" + lives;
+            //game partial reset
+        } else {
+            //gameOver
+        }
+    }
 }
