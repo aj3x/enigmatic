a67d1402750164f9ed3a5c7ff0609dfb7e55d5d6
aj3x <ajrussell94@hotmail.com>
2015-11-19 10:59:29 -0600

    Xuke's Merge
    Everything in here has been taken from Xuke's portion of the project
    -Castle design
    -Player Movement
    -Player teleport

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerControl : MonoBehaviour
+{
+
+    public float speed;
+
+    private CharacterController controller;
+
+    void Start()
+    {
+
+        controller = GetComponent<CharacterController>();
+
+    }
+
+    void Update()
+    {
+
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            //vector3.right for x axis, vector3.up for y axis, and vector3.forward for z axis, 
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            transform.eulerAngles = new Vector3(0, 0);
+        }
+
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(-Vector3.forward * speed * Time.deltaTime);
+            transform.eulerAngles = new Vector3(0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-Vector2.right * speed * Time.deltaTime);
+            transform.eulerAngles = new Vector3(0, 0);
+        }
+
+        //rotate right
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+            transform.eulerAngles = new Vector3(0, 0);
+        }
+    }
+}
